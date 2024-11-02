edd832b0bdb945bfabd12064d0b1f3b2a0187004
Aykhan Hagverdili <aykhanhagverdili@gmail.com>
2024-11-02 18:52:55 +0100

    Add sysup abbreviation

--- commands.fish
+++ commands.fish
@@ -1,5 +1,11 @@
 #!/usr/bin/fish
 
+# Copy into ~/.config/fish/config.fish
+
+
+# Update the system
+abbr -a sysup "sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y"
+
 
 for script in $(dirname $(status --current-filename))/scripts/*.fish
     source $script
